76ccb3d9adf45b31dcfea64d10c80bbb22bdb229
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-08-20 16:26:27 +0200

    setup autodoc

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject ring-basic-authentication "0.0.0-SNAPSHOT"
   :description "Ring middleware for basic authentication."
+  
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-base64 "0.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [autodoc "0.7.1"]]
+  
+  :autodoc {:description "Ring middleware to enforce basic authentication as described in RFC2617 section 2."
+            :copyright "Copyright (c) Remco van 't Veer."
+            :web-src-dir "http://github.com/remvee/ring-basic-authentication/blob/"})
--- src/remvee/ring/middleware/basic_authentication.clj
+++ src/remvee/ring/middleware/basic_authentication.clj
@@ -1,5 +1,14 @@
+;; Copyright (c) Remco van 't Veer. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution.  By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license.  You must not remove this notice, or any other, from
+;; this software.
+
 (ns remvee.ring.middleware.basic-authentication
   "HTTP basis authentication middleware for ring."
+  {:author "Remco van 't Veer"}
   (:use clojure.test
         [remvee.base64 :as base64]))
 

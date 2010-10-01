7b838845a5c4a4ed1f28ec1081c09b79aecf6d79
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-10-01 15:24:48 +0200

    drop matcher argument (use other middleware to do your matching) reordered arguments and added denied-response to allow fancy authorization failure response

--- src/remvee/ring/middleware/basic_authentication.clj
+++ src/remvee/ring/middleware/basic_authentication.clj
@@ -16,56 +16,76 @@
   "Wrap response with a basic authentication challenge as described in
   RFC2617 section 2.
 
-  The realm is a descriptive string visible to the visitor.  It,
-  together with the canonical root URL, defines the protected resource
-  on the server.
-
   The authenticate function is called with two parameters, the
   username and password, and should return a value when the login is
   valid.
 
-  The optional matcher function determines if authorization is
-  required.  Is receives the original request map."
+  The realm is a descriptive string visible to the visitor.  It,
+  together with the canonical root URL, defines the protected resource
+  on the server.
+
+  The denied-response is a ring response structure which will be
+  returned when authorization fails.  The appropriate status and
+  authentication headers will be merged into it.  It defaults to plain
+  text 'access denied' response."
   
   {:test
    (fn []
-     (let [f (wrap-basic-authentication (fn [_] :pass)
-                                        "test realm"
-                                        #(and (= %1 "tester")
-                                              (= %2 "secret")))]
-       (let [r (f {:headers {}})]
+     (do
+       ;; authorization success
+       (is (= :pass ((wrap-basic-authentication (fn [_] :pass)
+                                                #(and (= %1 "tester")
+                                                      (= %2 "secret")))
+                     {:headers {"authorization"
+                                (str "Basic " (base64/encode-str "tester:secret"))}})))
+
+       ;; authorization failure
+       (let [f (wrap-basic-authentication (fn [_] :pass)
+                                          #(and (= %1 "tester")
+                                                (= %2 "secret")))
+             r (f {:headers {}})]
          (is (= 401 (:status r)))
-         (is (= "Need authorization .." (:body r)))
-         (is (not (empty? (re-find #"test realm"
-                                   (get (:headers r)
-                                        "WWW-Authenticate"))))))
-       (let [r (f {:headers
-                   {"authorization"
-                    (str "Basic "
-                         (base64/encode-str "tester:secret"))}})]
-         (is (= :pass r)))))}
-  
-  ([app realm authenticate]
-     (wrap-basic-authentication app realm authenticate nil))
-  ([app realm authenticate matcher]
+         (is (= "access denied" (:body r)))
+         (is (re-matches #".*\"restricted area\"" (get (:headers r) "WWW-Authenticate"))))
+
+       ;; fancy authorization failure
+       (let [f (wrap-basic-authentication (fn [_] :pass)
+                                          #(and (= %1 "tester")
+                                                (= %2 "secret"))
+                                          "test realm"
+                                          {:headers {"Content-Type" "test/mime"}
+                                           :body "test area not accessable"})]
+         (let [r (f {:headers {}})]
+           (is (= 401 (:status r)))
+           (is (= "test area not accessable" (:body r)))
+           (is (= "test/mime" (get (:headers r) "Content-Type")))
+           (is (get (:headers r) "WWW-Authenticate"))
+           (is (re-matches #".*\"test realm\"" (get (:headers r) "WWW-Authenticate")))))))}
+
+  ([app authenticate]
+     (wrap-basic-authentication app authenticate nil nil))
+  ([app authenticate realm]
+     (wrap-basic-authentication app authenticate realm nil))
+  ([app authenticate realm denied-response]
      (fn [req]
-       (if (and matcher (not (matcher req)))
-         (app req)
-         (let [auth ((:headers req) "authorization")
-               cred (and auth
-                         (base64/decode-str
-                          (last
-                           (re-find #"^Basic (.*)$" auth))))
-               name (and cred
-                         (last
-                          (re-find #"^(.*):" cred)))
-               pass (and cred
-                         (last
-                          (re-find #":(.*)$" cred)))]
-           (if (authenticate name pass)
-             (app req)
-             {:status  401
-              :headers {"WWW-Authenticate" (format "Basic realm=\"%s\""
-                                                   realm)}
-              :body    "Need authorization .."}))))))
+       (let [auth ((:headers req) "authorization")
+             cred (and auth
+                       (base64/decode-str
+                        (last
+                         (re-find #"^Basic (.*)$" auth))))
+             name (and cred
+                       (last
+                        (re-find #"^(.*):" cred)))
+             pass (and cred
+                       (last
+                        (re-find #":(.*)$" cred)))]
+         (if (authenticate name pass)
+           (app req)
+           (assoc (merge {:headers {"Content-Type" "text/plain"}
+                          :body    "access denied"}
+                         denied-response)
+             :status  401
+             :headers (merge (:headers denied-response)
+                             {"WWW-Authenticate" (format "Basic realm=\"%s\""
+                                                         (or realm "restricted area"))})))))))
 

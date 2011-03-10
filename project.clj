8530855f5fbed2d0becb744a605c26c2e1fcf596
Remco van 't Veer <rwvtveer@xs4all.nl>
2011-03-10 08:17:34 +0100

    release 0.0.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ring-basic-authentication "0.0.1-SNAPSHOT"
+(defproject ring-basic-authentication "0.0.1"
   :description "Ring middleware for basic authentication."
   
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-base64 "0.0.0-SNAPSHOT"]]
+                 [clj-base64 "0.0.1"]]
   
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [autodoc "0.7.1"]]

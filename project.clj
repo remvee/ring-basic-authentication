de1c9b779a68b80e3a193e015fcbc4fbe6ba9280
Remco van 't Veer <remco@remworks.net>
2014-02-16 11:46:56 +0100

    Use codox instead of autodoc for apidoc generation

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject ring-basic-authentication "1.0.5"
-  :description "Ring middleware for basic authentication."
+  :description "Ring middleware to enforce basic authentication as described in RFC2617 section 2."
+  :url "https://github.com/remvee/ring-basic-authentication/"
+
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
   
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.codec "0.1.0"]]
   
-  :autodoc {:description "Ring middleware to enforce basic authentication as described in RFC2617 section 2."
-            :copyright "Copyright (c) Remco van 't Veer."
-            :web-src-dir "http://github.com/remvee/ring-basic-authentication/blob/"})
+  :codox {:src-dir-uri "https://github.com/remvee/ring-basic-authentication/blob/master/"
+          :src-linenum-anchor-prefix "L"})

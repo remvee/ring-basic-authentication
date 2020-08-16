821115379363dc952170d1796ef4f009ef424ad0
Remco van 't Veer <remco@remworks.net>
2020-08-16 16:59:42 +0200

    Update codox

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "https://github.com/remvee/ring-basic-authentication/"
 
   :license {:name "Eclipse Public License - v 1.0"
-            :url "http://opensource.org/licenses/eclipse-1.0.php"}
-  
+            :url  "http://opensource.org/licenses/eclipse-1.0.php"}
+
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  
-  :codox {:src-dir-uri "https://github.com/remvee/ring-basic-authentication/blob/master/"
-          :src-linenum-anchor-prefix "L"})
+  :plugins [[lein-codox "0.10.7"]]
+
+  :codox {:output-path    "doc"
+          :source-dir-uri ""
+          :source-uri     "https://github.com/remvee/ring-basic-authentication/blob/{git-commit}/{filepath}#L{line}"})

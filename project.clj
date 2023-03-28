1b248697933f686dea3081f00e890b589c886b3a
Remco van 't Veer <remco@remworks.net>
2023-03-28 10:02:00 +0200

    Add scm to project.clj for cljdoc

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ring-basic-authentication "1.1.1"
   :description "Ring middleware to enforce basic authentication as described in RFC2617 section 2."
   :url "https://git.sr.ht/~rwv/ring-basic-authentication"
+  :scm {:name "git" :url "https://git.sr.ht/~rwv/ring-basic-authentication"}
 
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://opensource.org/licenses/eclipse-1.0.php"}
